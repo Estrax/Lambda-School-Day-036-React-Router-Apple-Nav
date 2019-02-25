8b5f946dab2a5525818020c8c17b7032ea5ce0a9
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-25 18:04:47 -0500

    added basic styles with styled components

---
+++ apple-nav/src/styles/index.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const TopNav = styled.nav`
+    height: 50px;
+    background: rgb(45, 45, 45);
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const TopNavContent = styled.div`
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const NavItem = styled.div`
+    color: white;
+    text-decoration: none;
+`;
